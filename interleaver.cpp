3950e51cde3e065ce64c64be38c6415975dac2d0
malikkurtz <malikkurtz129@gmail.com>
2024-11-09 19:13:57 -0500

    This is very painful

--- interleaver.cpp
+++ interleaver.cpp
@@ -1,6 +1,39 @@
 #include <vector>
 #include <iostream>
 
+std::vector<bool> stringToVecBool(const std::string& code) {
+
+    std::vector<bool> toReturn = {};
+
+    std::bitset<8> curCharacter;
+
+    // for every character in the string
+    for (char c : code) {
+        curCharacter = c;
+        // for every bit in the character
+        for (int i = 7; i >= 0; i--) {
+            toReturn.push_back(curCharacter[i]);
+        }
+    }
+
+    return toReturn;
+}
+
+std::string vecBoolToString(const std::vector<bool>& vector_code) {
+    std::string toReturn = "";
+
+    char c = 0;
+
+    for (int i = 0; i < vector_code.size() ; i+=8) {
+        for (int j = 0; j < 8 ; j++) {
+            c = c << 1 | vector_code[i+j];
+        }
+        toReturn += c;
+    }
+
+    return toReturn;
+}
+
 void printRegisters(std::vector<std::vector<bool>> registers, int timeStep) {
     std::cout << "Registers at TimeStep " << timeStep << std::endl;
     std::cout << "[";
@@ -28,7 +61,7 @@ std::vector<bool> interleave(std::vector<bool> input, int burst_protection) {
     int timeStep = 0;
     
 
-    auto pointerToNextDataBitToProcess = input.begin();
+    // auto pointerToNextDataBitToProcess = input.begin();
 
     // pad with zeroes at the end
     while (input.size() % N != 0) {
@@ -49,7 +82,7 @@ std::vector<bool> interleave(std::vector<bool> input, int burst_protection) {
 
 
     // while we have not yet processed each bit in input
-    while (iterator < pointerToEnd) {
+    while (iterator != pointerToEnd) {
         // Start Shift the registers
         for (int i = 0; i < N-1; i++) {
                 // First register will be equal to the second register etc
@@ -59,8 +92,8 @@ std::vector<bool> interleave(std::vector<bool> input, int burst_protection) {
             }
         // set the last register to the N new input bits incoming
         for (int i = 0; i < N; i++) {
-            registers[N-1][i] = *pointerToNextDataBitToProcess;
-            pointerToNextDataBitToProcess++;
+            registers[N-1][i] = *iterator;
+            iterator++;
         }
 
         
@@ -70,10 +103,19 @@ std::vector<bool> interleave(std::vector<bool> input, int burst_protection) {
 
 
         // Grab each appropriate bit from each shift register according to its delay
-        for (int i = 0; i < registers.size(); i++) {
-            interleaved.push_back(registers[i][N - 1 - i]);
-            iterator++;
+        if (timeStep >= (N-1)) {
+            for (int i = 0; i < registers.size(); i++) {
+                interleaved.push_back(registers[i][N - 1 - i]);
+            }
         }
+        else {
+            //timeStep = 2
+
+            for (int i = (N-1) - timeStep; i <= (N-1); i++) {
+                interleaved.push_back(registers[i][(N-1) - i]);
+            }
+        }
+        
 
         
         printInterleaved(interleaved);
@@ -88,13 +130,15 @@ std::vector<bool> interleave(std::vector<bool> input, int burst_protection) {
 }   
 
 int main(){
+    std::string message = "Hello World!";
+    std::vector<bool> toInterleave = stringToVecBool(message);
 
-    std::vector<bool> inter = interleave({1,1,0,0,1,1,0,0,1,1}, 4);
+    toInterleave = {1,1,0,0,1,1,0,0,1,1};
+    // toInterleave = {1,1,0,0,1,1,0,0,1,1,0,0,0,1,1,1,0,0,0,1,1,1};
+    std::vector<bool> interleaved = interleave(toInterleave, 3);
 
-    std::cout << "[";
-    for (bool b : inter) {
-        std::cout << b;
-    } std::cout << "]" << std::endl;
+    // std::cout << "After Interleaving: " << vecBoolToString(interleaved) << std::endl;
+
 
     return 0;
 }
